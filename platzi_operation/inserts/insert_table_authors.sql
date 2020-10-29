b8c1b87abfa4a9ae49201dd6069ab66a28521151
resparzasoto <rubenesparzasoto95@gmail.com>
2020-10-29 11:22:24 -0700

    Comando INSERT

---
+++ platzi_operation/inserts/insert_table_authors.sql
@@ -0,0 +1,16 @@
+-- Single row insert
+INSERT INTO authors (name, nationality)
+VALUES ('Juan Rulfo', 'MEX');
+
+INSERT INTO authors (name, nationality)
+VALUES ('Gabriel García Márquez', 'COL');
+
+INSERT INTO authors (name, nationality)
+VALUES ('Juan Gabriel Vasquez', 'COL');
+
+-- Multiple rows insert
+INSERT INTO authors (name, nationality)
+VALUES  ('Julio Cortázar', 'ARG'),
+        ('Isabel Allende', 'CHI'),
+        ('Octavio Paz', 'MEX'),
+        ('Juan Carlos Onetti', 'URU');
