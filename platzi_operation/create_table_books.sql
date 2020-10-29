cf35c784ac066d9f66a2923aa21a7ac7dcc0abf1
resparzasoto <rubenesparzasoto95@gmail.com>
2020-10-28 22:31:47 -0700

    Tipos de columnas / Creación de la tabla books

---
+++ platzi_operation/create_table_books.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS books (
+    book_id INTEGER UNSIGNED PRIMARY AUTO_INCREMENT,
+    author ,
+    title VARCHAR(100) NOT NULL,
+    year INTEGER UNSIGNED NOT NULL DEFAULT 1900,
+    languages VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'ISO 639-1 Language',
+    cover_url VARCHAR(500),
+    price DOUBLE(6,2) NOT NULL DEFAULT 10.0,
+    sellable TINYINT(1) DEFAULT 1,
+    copies INTEGER NOT NULL DEFAULT 1,
+    description TEXT
+);
